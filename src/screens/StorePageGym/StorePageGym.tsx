b83dff1f05df3f1f126edad7355162a6bd55ac9f
Arsalan Ahmed <134292213+ArsalanAhmed03@users.noreply.github.com>
2025-04-30 21:07:54 +0500

    StorePageAdded

---
+++ src/screens/StorePageGym/StorePageGym.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { StorePage1 } from "../../components/StorePage1";
+import { StorePage2 } from "../../components/StorePage2";
+
+export const StorePageGym = (): JSX.Element => {
+  return (
+    <div
+      className="flex flex-col items-center gap-4 px-20 py-0 relative [background:url(..//bg1.png)_50%_50%_/_cover] min-h-screen"
+      data-model-id="91:1046"
+    >
+      <div className="flex flex-col items-start gap-2 relative self-stretch w-full flex-[0_0_auto] bg-[#00000080]">
+        <div className="flex flex-col items-center gap-8 px-4 py-10 relative self-stretch w-full flex-[0_0_auto]">
+          <h1 className="font-['Roboto',Helvetica] font-bold text-gray-300 text-3xl text-center tracking-[0] leading-9">
+            ---
+          </h1>
+          <div className="flex items-center justify-center gap-2 px-[159px] py-0 relative self-stretch w-full flex-[0_0_auto]">
+            <h1 className="relative w-fit mt-[-1.00px] [font-family:'Roboto',Helvetica] font-bold text-white text-3xl text-center tracking-[0] leading-9 whitespace-nowrap">
+              Gym Merchandise &amp; Supplements
+            </h1>
+          </div>
+
+          <StorePage1 />
+          <StorePage2 />
+        </div>
+      </div>
+    </div>
+  );
+};
